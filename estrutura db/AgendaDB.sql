a97508d36d5ae26ccc98af4f34f88c5fe9e14b27
mariaebonifacio <madubonifacio12@gmail.com>
2024-10-29 10:17:59 -0300

    Add: Banco de dados

---
+++ estrutura db/AgendaDB.sql
@@ -0,0 +1,8 @@
+create database dbAgenda;
+use dbAgenda;
+create table tb_usuarios{
+nome varchar(60) not null,
+usuario varchar(20) PRIMARY KEY,
+telefone varchat(15) not null,
+senha varchar(20)
+);
