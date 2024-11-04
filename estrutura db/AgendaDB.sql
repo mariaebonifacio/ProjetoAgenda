fcf0c4310d58d505ad58a7775da4158dd746bcea
mariaebonifacio <madubonifacio12@gmail.com>
2024-11-04 08:27:45 -0300

    Add: banco de dados

--- estrutura db/AgendaDB.sql
+++ estrutura db/AgendaDB.sql
@@ -1,8 +1,8 @@
-create database dbAgenda;
-use dbAgenda;
-create table tb_usuarios{
+create database bdagenda;
+use bdagenda;
+create table tbusuarios(
 nome varchar(60) not null,
 usuario varchar(20) PRIMARY KEY,
-telefone varchat(15) not null,
+telefone varchar(15) not null,
 senha varchar(20)
 );
